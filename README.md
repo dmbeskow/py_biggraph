4e5c9db202739fecbf2299a1e5b4bdd5f62109c9
David Beskow <dbeskow@GS17928.SP.CS.CMU.EDU>
2020-04-01 15:28:47 -0400

    added config

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# py_biggraph
+# Embed Pandas Edgelist Graph with Pytorch Biggraph
+
+This package takes a tutorial found at [here](https://github.com/facebookresearch/PyTorch-BigGraph/blob/master/torchbiggraph/examples/livejournal.py) and makes it easy to apply to a Graph that is represented in a Pandas Edgelist.  
+
---
+++ config.py
@@ -0,0 +1,34 @@
+entities_base = 'data/project'
+
+def get_torchbiggraph_config():
+
+    config = dict(
+        # I/O data
+        entity_path=entities_base,
+        edge_paths=[],
+        checkpoint_path='model/project',
+
+        # Graph structure
+        entities={
+            'user_id': {'num_partitions': 1},
+        },
+        relations=[{
+            'name': 'follow',
+            'lhs': 'user_id',
+            'rhs': 'user_id',
+            'operator': 'none',
+        }],
+
+        # Scoring model
+        dimension=1024,
+        global_emb=False,
+
+        # Training
+        num_epochs=8,
+        lr=0.001,
+
+        # Misc
+        hogwild_delay=2,
+    )
+
+    return config
